6bd8c2ce640d9703e5073d6fe1baf52839f3ece7
Shannon <skotraba@gmail.com>
2021-03-25 10:24:42 -0400

    Added show/hide button function

--- src/components/Input/InputHolder.js
+++ src/components/Input/InputHolder.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import { Button, Collapse } from '@material-ui/core';
 
@@ -9,10 +9,59 @@ import InputGet from './InputGet';
 import '../Card/Card.scss';
 
 export default function InputHolder(props) {
+
+  const [open, setOpen ] = useState(false)
+
+  // if(!open){
+  //   return (
+  //     <div className="test">
+  //       <Collapse in={!open}>
+  //        <Button className="myBtn" size="medium" variant="contained"
+  //        onClick={(() => setOpen(true))}
+  //        >Add Card</Button>
+  //       </Collapse>
+  //   </div>
+  //   )
+  // }
+
+  // return (
+  //   <div className="test">
+  //     <Collapse in={open}>
+  //       <Button 
+  //       className="myBtn"
+  //        size="medium" 
+  //        variant="contained"
+  //        onClick={(() => setOpen(!open))}
+  //        >Add Card</Button>
+  //       <Collapse in={open}>
+  //         <InputGet/>
+  //       </Collapse>
+  //     </Collapse>
+      
+  //   </div>
+  // );
+
+
   return (
     <div className="test">
-      <Button className="myBtn" size="medium" variant="contained">Add Card</Button>
-      <InputGet></InputGet>
+      <Collapse in={open}>
+        <InputGet setOpen={setOpen}></InputGet>
+        <Button 
+        className="myBtn"
+         size="medium" 
+         variant="contained"
+         onClick={(() => setOpen(!open))}
+         >Add Card</Button>
+      </Collapse>
+      <Collapse in={!open}>
+      <Button 
+        className="myBtn"
+         size="medium" 
+         variant="contained"
+         onClick={(() => setOpen(!open))}
+         >Add Card</Button>
+      </Collapse>
+
     </div>
-  );
+  )
 }
