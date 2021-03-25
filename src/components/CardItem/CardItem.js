6fdaada099544be840bb5654a7684282b6b71b5b
Shannon <skotraba@gmail.com>
2021-03-24 22:02:01 -0400

    Changed styles

---
+++ src/components/CardItem/CardItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../Card/Card.scss';
+
+export default function CardItem(props) {
+  return (
+    <div className="cardItem">
+      <div className="cardItem__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. </div>
+    </div>
+  );
+}
